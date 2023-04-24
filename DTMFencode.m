2db953003152196a86a6444cd934536a988a4eed
Wilson Martinez Jr <wilsonmar27@gmail.com>
2023-04-24 14:40:53 -0400

    modify DTMFencode to be more memory effecient

--- DTMFencode.m
+++ DTMFencode.m
@@ -46,35 +46,27 @@ if ~ismember(key, digits)
     error("Invalid key entry")
 end
 
-% Create basis waves
-secs = duration/1000;
-samples = secs*fs;                  % Smaple amount
-ind = linspace(0, secs, samples);   % Independent variable
-waves = zeros(7,samples);           % allocate array
-
 freqs = [697 770 852 941 1209 1336 1477];
-
-% initialize the basis waves
-for i = 1:7
-    waves(i,:) = sin((freqs(i)*2*pi).*ind);
-end
 
 % row select
 for i = 1:4
     if ismember(key, digits(i,:))
-        wave1 = waves(i,:);
+        w1freq = freqs(i);
     end
 end
 
 % column select
 for i = 1:3
     if ismember(key, digits(:,i))
-        wave2 = waves(i+4,:);
+        w2freq = freqs(i+4);
     end
 end
 
-% output wave
-x = weight(1).*wave1 + weight(2).*wave2;
+secs = duration/1000;               % change from ms to s
+samples = secs*fs;                  % Smaple amount
+ind = linspace(0, secs, samples);   % Independent variable
 
+% output wave
+x = weight(1)*sin((w1freq*2*pi).*ind) + weight(2)*sin((w2freq*2*pi).*ind);
 
 return
