e12a1a62dcb9b466e0c88780fb9172984379e170
Wilson Martinez Jr <wilsonmar27@gmail.com>
2023-04-25 20:01:23 -0400

    finsih implementing sequence (pt3)

--- DTMFsequence.m
+++ DTMFsequence.m
@@ -10,37 +10,79 @@ function [seq,fs] = DTMFsequence(filename)
 %   seq:    a sequence of character corresponding to one of twelve possible keys
 %   fs:     sampling rate in Hz
 
+% rms scan window size in s
+% movement of window is half the window size
+WINDOW_SIZE = 0.02;
+RMS_THRESH = 0.2;
 
-% load the audio file
 [x,fs] = audioread(filename);
 
-% frame sizes
-frame_size = (250/1000) * fs;
-frame_step = (125/1000) * fs;
+samples = length(x);
+secs = samples/fs;
+ind = linspace(0, secs, samples);   % Independent variable
 
-% divide the signal into these frames 
-num_frames = floor((length(x)-frame_size)/frame_step)+1;
-frames = zeros(num_frames, frame_size);
-for i = 1:num_frames
-    frame_start = (i-1)*frame_step+1;
-    frame_end = frame_start+frame_size-1;
-    frames(i,:) = x(frame_start:frame_end);
+% scale
+limits = [max(x), abs(min(x))];
+x = x/(max(limits));
+
+% detect changes in rms to trace out the wave
+rmswindow = WINDOW_SIZE * fs;
+rmssize = ceil(samples/rmswindow*2) + 1;
+rmsvals = zeros(1,rmssize);
+
+% trace
+for i = 1:rmssize
+    beg = rmswindow*(i-1)/2+1;
+    fin = rmswindow*i/2;
+    if fin > samples
+        fin = samples;
+    end
+    rmsvals(i) = rms(x(beg:fin));
+end
+
+% scale rms wave
+rmsvals = rmsvals/max(rmsvals);
+% shift rms wave
+rmsvals = [0, rmsvals(1:end-1)];
+% rms wave time scale
+rmstime = linspace(0,secs,rmssize);
+
+% create beep
+% beep is one wherever there is a phone beep
+% allocate array
+beep_inter = zeros(15, 2);
+beep = zeros(1,rmssize);
+curr_row = 1;
+
+for i = 2:rmssize-1
+    left_neighbor = (rmsvals(i-1) > RMS_THRESH);
+    right_neighbor = (rmsvals(i+1) > RMS_THRESH);
+
+    % beeps should be at least of length 2 
+    if (rmsvals(i) > RMS_THRESH) && (left_neighbor || right_neighbor)
+        beep(i) = 1;
+        % where beep starts
+        if beep(i-1) == 0
+            beep_inter(curr_row, 1) = i-1;
+        end
+    else
+        % where beep ends
+        if beep(i-1) == 1
+            beep_inter(curr_row, 2) = i;
+            curr_row = curr_row + 1;
+        end
+    end
 end
 
 seq = "";
-
-% for each frame, detect what value it is
-for i=1:num_frames
-
-    % Save audio file
-    filenameAudio = ['./temp', frames(i), '.wav'];
-    limits = [max(x), abs(min(x))];
-    x = x/(max(limits));
-    audiowrite(filenameAudio,x,fs);
-
-    % use decode function to find key
-    key = DTMFdecode('./temp.wav');
-    strcat(seq, key);
+clear i;
+for i = 1:length(beep_inter)
+    if beep_inter(i,1) ~= 0
+        startbeep = floor(beep_inter(i,1)*(samples/rmssize));
+        endbeep = ceil(beep_inter(i,2)*(samples/rmssize));
+        key = DTMFdecodeSignal(x(startbeep:endbeep), fs);
+        seq = append(seq, key);
+    end
 end
 
 end
--- test2.m
+++ test2.m
@@ -1,8 +1,9 @@
-[x,fs] = audioread('DTMFex/dtmf1.wav');
+[x,fs] = audioread('DTMFex/dtmf19.wav');
 
 % rms scan window size in s
 % movement of window is half the window size
 WINDOW_SIZE = 0.02;
+RMS_THRESH = 0.2;
 
 samples = length(x);
 secs = samples/fs;
@@ -17,6 +18,7 @@ rmswindow = WINDOW_SIZE * fs;
 rmssize = ceil(samples/rmswindow*2) + 1;
 rmsvals = zeros(1,rmssize);
 
+% trace
 for i = 1:rmssize
     beg = rmswindow*(i-1)/2+1;
     fin = rmswindow*i/2;
@@ -33,7 +35,55 @@ rmsvals = [0, rmsvals(1:end-1)];
 % rms wave time scale
 rmstime = linspace(0,secs,rmssize);
 
+% create beep
+% beep is one wherever there is a phone beep
+% allocate array
+beep_inter = zeros(15, 2);
+beep = zeros(1,rmssize);
+curr_row = 1;
+
+for i = 2:rmssize-1
+    left_neighbor = (rmsvals(i-1) > RMS_THRESH);
+    right_neighbor = (rmsvals(i+1) > RMS_THRESH);
+
+    % beeps should be at least of length 2 
+    if (rmsvals(i) > RMS_THRESH) && (left_neighbor || right_neighbor)
+        beep(i) = 1;
+        % where beep starts
+        if beep(i-1) == 0
+            beep_inter(curr_row, 1) = i-1;
+        end
+    else
+        % where beep ends
+        if beep(i-1) == 1
+            beep_inter(curr_row, 2) = i;
+            curr_row = curr_row + 1;
+        end
+    end
+end
+
+% decode each beep
+nums = "";
+clear i;
+for i = 1:length(beep_inter)
+    if beep_inter(i,1) ~= 0
+        startbeep = floor(beep_inter(i,1)*(samples/rmssize));
+        endbeep = ceil(beep_inter(i,2)*(samples/rmssize));
+        key = DTMFdecodeSignal(x(startbeep:endbeep), fs);
+        nums = append(nums, key);
+    end
+end
+
+
+figure('position', [120,360,1200,480]);
 plot(ind,x);
 hold on
-plot(rmstime, rmsvals, 'LineWidth', 1)
-legend("Sound Wave", "Trace");
+plot(rmstime, rmsvals, 'LineWidth', 1, 'Color', "g");
+plot(rmstime, beep, 'lineWidth', 1, 'Color', "r");
+legend("Sound Wave", "Trace", "Beep");
+title("Beep Detection on dtmf15.wav");
+ylabel("Value");
+xlabel("Time (s)");
+% saveas(gcf, "assets/BeepDetect.png");
+
+strlength(nums)
